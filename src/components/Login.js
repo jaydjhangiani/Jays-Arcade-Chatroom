2df967d01455a082162ee2beac37ddfd7811be06
Jay Jhangiani <47749517+jaydjhangiani@users.noreply.github.com>
2021-06-15 16:41:38 +0530

    updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,115 +1,124 @@
-import {auth} from '../firebase'
+import { auth } from "../firebase";
 // import logo from '../assets/img/releaf.jpg'
-import styled from 'styled-components'
-import { useState } from 'react'
+import styled from "styled-components";
+import { useState } from "react";
 function Login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
 
-    const [email,setEmail] = useState()
-    const [password,setPassword] = useState()
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((e) => alert(e.message));
+  };
 
-    const handleSignIn = (e) => {
-        e.preventDefault()
-        auth.signInWithEmailAndPassword( email,password )
-        .catch((e) => alert(e.message));
-    }
-
-
-    return (
-
-        <LoginContainer>
-            {/* <LoginImg src={logo} alt="Releaf"/> */}
-            {/* <h1>Welcome to the Chat Room</h1> */}
-            <FormContent>
-                <Form action ="#">
-                    <FormH1>The Retro Arcade Chat Room</FormH1>
-                    <FormLabel htmlFor="for">Email</FormLabel>
-                    <FormInput type="email" required onChange={e => setEmail(e.target.value)}/>
-                    <FormLabel htmlFor="for">Password</FormLabel>
-                    <FormInput type="password" required onChange={e => setPassword(e.target.value)} />
-                    <FormButton onClick={handleSignIn} type="submit">Continue</FormButton>
-                    <Text href="https://jays-arcade-2eec1.firebaseapp.com/" >Don't have an account? Sign up!</Text>
-                </Form>
-            </FormContent>
-            {/* <Button onClick={handleSignIn}>Sign In</Button> */}
-        </LoginContainer>
-    )
+  return (
+    <LoginContainer>
+      {/* <LoginImg src={logo} alt="Releaf"/> */}
+      {/* <h1>Welcome to the Chat Room</h1> */}
+      <FormContent>
+        <Form action="#">
+          <FormH1>Jay's Arcade Chat Room</FormH1>
+          <FormLabel htmlFor="for">Email</FormLabel>
+          <FormInput
+            type="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <FormLabel htmlFor="for">Password</FormLabel>
+          <FormInput
+            type="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FormButton onClick={handleSignIn} type="submit">
+            Continue
+          </FormButton>
+          <Text href="https://jays-arcade-2eec1.firebaseapp.com/">
+            Don't have an account? Sign up!
+          </Text>
+        </Form>
+      </FormContent>
+      {/* <Button onClick={handleSignIn}>Sign In</Button> */}
+    </LoginContainer>
+  );
 }
 
-export default Login
+export default Login;
 
 const LoginContainer = styled.div`
-    /* display: grid; */
-    height: 100vh;
-    width: 100%;
-    /* background-color: #7887CB; */
-    background: #010101;
+  /* display: grid; */
+  height: 100vh;
+  width: 100%;
+  /* background-color: #7887CB; */
+  background: #010101;
 `;
 
 const Text = styled.a`
-    text-align: center;
-    margin-top: 24px;
-    color: #fff;
-    font-size: 14px;
+  text-align: center;
+  margin-top: 24px;
+  color: #fff;
+  font-size: 14px;
 `;
 
 const FormContent = styled.div`
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    @media screen and (max-width: 480px){
-        padding: 10px;
-    }
-`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  @media screen and (max-width: 480px) {
+    padding: 10px;
+  }
+`;
 
 const Form = styled.form`
-    
-    background-color: #7887CB;
-    max-width: 400px;
-    height: auto;
-    /* width: 100%; */
-    z-index: 1;
-    display: grid;
-    margin: 0 auto;
-    padding: 80px 32px;
-    border-radius: 4px;
-    box-shadow: 0 1px 3px rgba(0,0,0,0.9);
-    @media screen and (max-width: 480px){
-        padding: 32px 32px;
-    }
-`
+  background-color: #7887cb;
+  max-width: 400px;
+  height: auto;
+  /* width: 100%; */
+  z-index: 1;
+  display: grid;
+  margin: 0 auto;
+  padding: 80px 32px;
+  border-radius: 4px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.9);
+  @media screen and (max-width: 480px) {
+    padding: 32px 32px;
+  }
+`;
 
 const FormH1 = styled.h1`
-    margin-bottom: 40px;
-    color: #fff;
-    font-size: 1.4rem;
-    font-weight: 600;
-    text-align: center;
-`
+  margin-bottom: 40px;
+  color: #fff;
+  font-size: 1.4rem;
+  font-weight: 600;
+  text-align: center;
+`;
 
 const FormLabel = styled.label`
-    margin-bottom: 8px;
-    font-size: 14px;
-    color: #fff;
-`
+  margin-bottom: 8px;
+  font-size: 14px;
+  color: #fff;
+`;
 
 const FormInput = styled.input`
-    padding: 16px 16px;
-    margin-bottom: 32px;
-    border: none;
-    border-radius: 4px;
-`
+  padding: 16px 16px;
+  margin-bottom: 32px;
+  border: none;
+  border-radius: 4px;
+`;
 
 const FormButton = styled.button`
-    /* background: #01bf71; */
-    background: #1a237e;
-    padding: 16px 0;
-    border: none;
-    border-radius: 4px;
-    color: #fff;
-    font-size: 20px;
-    cursor: pointer;
-`
+  /* background: #01bf71; */
+  background: #1a237e;
+  padding: 16px 0;
+  border: none;
+  border-radius: 4px;
+  color: #fff;
+  font-size: 20px;
+  cursor: pointer;
+`;
 
 // const Button = styled.button`
 //     width: 300px;
--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -1,59 +1,56 @@
 import styled from "styled-components";
-import chatroom from '../assets/img/chatroom.svg'
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import chatroom from "../assets/img/chatroom.svg";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import { IconButton } from "@material-ui/core";
 
-const WelcomeScreen = ({isOpen, toggle}) => {
-    return (
-        <WelcomeContainer isOpen={isOpen}>
-            <WelcomeHeader>
-            <ShowSidebarButton>
-                <IconButton >
-                    <ArrowBackIosIcon onClick={toggle}/>
-                </IconButton>
-            </ShowSidebarButton>
-                <h4>
-                    Welcome To The Retro Arcade Chat Room
-                </h4>
-        </WelcomeHeader>
-        <WelcomeImg src={chatroom} />
-        </WelcomeContainer>
-    )
-}
+const WelcomeScreen = ({ isOpen, toggle }) => {
+  return (
+    <WelcomeContainer isOpen={isOpen}>
+      <WelcomeHeader>
+        <ShowSidebarButton>
+          <IconButton>
+            <ArrowBackIosIcon onClick={toggle} />
+          </IconButton>
+        </ShowSidebarButton>
+        <h4>Welcome To Jay's Arcade Chat Room</h4>
+      </WelcomeHeader>
+      <WelcomeImg src={chatroom} />
+    </WelcomeContainer>
+  );
+};
 
-export default WelcomeScreen
+export default WelcomeScreen;
 
 const WelcomeContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    flex: 0.65;
-    height: 100vh;
-    background-color: white;
+  display: flex;
+  flex-direction: column;
+  flex: 0.65;
+  height: 100vh;
+  background-color: white;
 
-    @media screen and (max-width:768px){
-        flex: 1;
-        display:${({isOpen}) => (isOpen ? 'none' : 'flex')};
-    }
+  @media screen and (max-width: 768px) {
+    flex: 1;
+    display: ${({ isOpen }) => (isOpen ? "none" : "flex")};
+  }
 `;
 
 const WelcomeHeader = styled.div`
-    padding: 20px;
-    align-items: center;
-    display: flex;
-    justify-content: space-around;
-    border-bottom: 1px solid lightgray;
-    background-color: #f5f5f5;
-    height: 30px;
+  padding: 20px;
+  align-items: center;
+  display: flex;
+  justify-content: space-around;
+  border-bottom: 1px solid lightgray;
+  background-color: #f5f5f5;
+  height: 30px;
 `;
 
 const ShowSidebarButton = styled.div`
-    display: none;
+  display: none;
 
-    @media screen and (max-width:768px){
-        display: block;
-    }
+  @media screen and (max-width: 768px) {
+    display: block;
+  }
 `;
-
 
 // const WelcomeBg = styled.div`
 //     position: absolute;
@@ -67,6 +64,6 @@ const ShowSidebarButton = styled.div`
 // `;
 
 const WelcomeImg = styled.img`
-    width: 80%;
-    margin: auto ;
+  width: 80%;
+  margin: auto;
 `;
